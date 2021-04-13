95a5fa6985e3f5f3f8b5e87b480fdcf305518dc3
Kiz9R <blankkizer@gmail.com>
2021-04-13 19:29:35 +0530

    master

---
+++ array1.py
@@ -0,0 +1,7 @@
+# TO IMPORT NUMPY IN VISUAL CODE JUST GO TO THE TERMINAL AND TYPE "pip3 install numpy" AND AGREE TO ANYTHING IT ASKS..
+import numpy as py
+v = py.array([1, 2, 4, 5])
+b = py.array([2, 4, 5, 6])
+d = v.copy()
+s = d + b
+print(s)
--- new.py
+++
@@ -1,3 +0,0 @@
-import numpy as py
-v = py.array('i', [1, 2, 3], [4, 5, 6])
-print(v)
